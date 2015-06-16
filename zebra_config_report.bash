21b7b6aedc81ece5db5ddff83aa852abc1b5fa21
Barton Chittenden <barton@bywatersolutions.com>
2015-06-16 08:50:21 -0400

    read ccl_properties from KOHA_CONF.
    Use xmlstarlet to read $ccl_properties from $KOHA_CONF.
    Also read $ZEBRA_BIBLIOS_DOM the same way.

--- zebra_config_report.bash
+++ zebra_config_report.bash
@@ -5,9 +5,6 @@
 # Usage: $0 [marcflavor]
 # If markflavor is not specified, it will default to 'marc21'. 
 
-# NOTE: the shell variabl3 ccl_properties is currently hard coded to 'koha-dev/etc/zebradb/ccl.properties'
-# You will need to change this on a package install.
-
 # Output should look something like this:
 # index name: 'ISBN' att: '7'
 # ===========================
@@ -22,7 +19,7 @@
 # TODO: MARCFLAVOR should be acquired from Koha.
 MARCFLAVOR=${1:-'marc21'}
 
-ZEBRA_BIBLIOS_DOM="$(grep zebra-biblios-dom.cfg $KOHA_CONF | sort -u | sed -e 's|[[:space:]]*</*config>||g')"
+ZEBRA_BIBLIOS_DOM="$(xmlstarlet sel -t -v 'yazgfs/server/config' $KOHA_CONF)"
 
 find_file() {
     for path in $(grep profilePath $1 | cut -d : -f2- | sed "s/:/\n/g"); do find $path ! -type d ; done | grep "$2"
@@ -36,14 +33,9 @@ underline ()
 
 bib1file=$(find_file $ZEBRA_BIBLIOS_DOM bib1.att)
 biblio_koha_indexdefs=$(find_file $ZEBRA_BIBLIOS_DOM "${MARCFLAVOR}.*biblio-koha-indexdefs.xml" )
-ccl_properties='koha-dev/etc/zebradb/ccl.properties'
-
-# TODO: ccl_properties can be found using
-#    grep 'ccl2rpn' $KOHA_CONF
+ccl_properties="$(xmlstarlet sel -t -v 'yazgfs/serverinfo/ccl2rpn' $KOHA_CONF)"
 
 # TODO: include biblio_zebra_indexdefs in the report.
-
-# TODO: better to use xpath rather than grep for grabbing values from xml files.
 
 grep '^att' $bib1file | while read placeholder att indexname
 do 
